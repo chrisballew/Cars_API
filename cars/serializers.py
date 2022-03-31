e9d1871ccfa2b16505405b05d0a0621a6ef27a3d
Chris <bluman325@gmail.com>
2022-03-31 11:20:16 -0500

    registered car model with admin site + completed POST functionality for Car + updated Rest Freamwork decimal settings

--- cars/serializers.py
+++ cars/serializers.py
@@ -4,4 +4,4 @@ from .models import Car
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        friends = ['id', 'make', 'model', 'year', 'price']
+        fields = ['id', 'make', 'model', 'year', 'price']
--- cars_project/urls.py
+++ cars_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/cars/', include('cars.url')),
+    path('api/cars/', include('cars.urls')),
 ]
